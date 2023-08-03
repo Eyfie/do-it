18a25190a9e3775f32b63ae02925f18ce879246b
Eyfie <thomas.mendezdevigo@gmail.com>
2023-08-03 18:01:31 +0200

    Basic set up

---
+++ src/TaskApp.tsx
@@ -0,0 +1,16 @@
+import { Box, Heading} from "@kuma-ui/core"
+
+
+function TaskApp() {
+
+
+  return (
+    <>
+      <Box as="main">
+        <Heading as="h1">Do it</Heading>
+      </Box>
+    </>
+  )
+}
+
+export default TaskApp
